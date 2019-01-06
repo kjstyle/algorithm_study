b6b655562b262cd835b62ff0b9a76938a69ccf86
kiljulee <kjstyle@tmon.co.kr>
2019-01-06 16:48:09 +0900

    코딩테스트.

--- src/main/java/kjstyle/woohyung/Test02.java
+++ src/main/java/kjstyle/woohyung/Test02.java
@@ -12,7 +12,59 @@ import java.util.stream.Collectors;
 import static java.util.Comparator.comparing;
 
 /**
- 날짜형태가 잘못되어 프로그램을 종료합니다 => " + i + "번째 열, 날짜 => " + cols[2]
+ John likes to travel. He has visited a lot of cities over many years.
+ Whenever he visits a city, he takes a few photos and saves them on his
+ computer. Each photo has a name with an extension ("jpg", "png" or
+ jpeg") and there is a record of the name of the city where the photo was
+ taken and the time and date the photo; for example: "photo.jpg,
+ Warsaw, 2013-09-05 14:08:15"
+ Note that, in some rare cases, photos from different locations may share
+ the time and date, due to timezone differences.
+ John notices that his way of filing photos on his computer has become a
+ mess. He wants to reorganize the photos. First he decides to group the
+ photos by city, then, within each such group, sort the photos by the time
+ they were taken and finally assign consecutive natural numbers to the
+ photos, starting from 1. Afterwards he intends to rename all the photos.
+ The new name of each photo should begin with the name of the city
+ followed by the number already assigned to that photo. The number of
+ every photo in each group should have the same length (equal to the lengt
+ of the largest number in this group), thus, John needs to add some leading
+ zeros to the numbers. The new name of the photo should end with the
+ extension, which should remain the same.
+ Your task is to help John by finding the new name of each photo.
+ Each of John's photos has the format: "<<photoname
+ <extension>>, <<city_name>>, yyyy-mm-dd hh :mm:ss", where
+ <photoname>>", <<extension>>"and "<<city name>>" consist
+
+
+ your function should return
+ Warsaw02.jpg
+ Londonl.png
+ Warsaw01.png
+ Paris2.jpg
+ Parisl.jpg
+ London2. jpg
+ Paris3.png
+ Warsawo 3.jpg
+ Warsawo9.png
+ Warsaw07. jpg
+ Warsaw06.ipg
+ Warsaw08.jpg
+ Warsaw04.png
+ Warsaw05.png
+ Warsawl0.jpg
+ as there are ten photos taken in Warsaw (numbered from 01 to 10), tw
+ photos in London (numbered from 1 to 2) and three photos in Paris
+ (numbered from 1 to 3)
+ The new names of the photos are returned in the same order as in the
+ given string
+ Assume that:
+ . M is an integer within the range [1.1001;
+ Each year is an integer within the range [2000..2020];
+ Each line of the input string is of the format
+ くくphotoname>>·<<extension>>, << city-name >> ,
+
+
  */
 public class Test02 {
 
